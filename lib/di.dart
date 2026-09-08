89070985d0aa5a7ec83fb4a11fb9c99958b0477b
林弈成 <linyicheng@linyichengdeMacBook-Air.local>
2026-09-08 17:11:28 +0800

    fix: import Riverpod legacy notifier provider

--- lib/di.dart
+++ lib/di.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 import 'core/audio/audio_clock.dart';
 import 'core/audio/input_capture.dart';
